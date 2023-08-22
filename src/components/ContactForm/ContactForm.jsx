a508d8bc099458c92180829d70a5e0f3b81a4a47
Maksym Baldyniuk <frundzik1989@gmail.com>
2023-08-22 19:21:48 +0300

    first commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component, useState } from 'react';
+import { Component } from 'react';
 import css from './ContactForm.module.css';
 
 export class ContactForm extends Component {
